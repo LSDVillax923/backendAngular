5a2a7792e300f2914c6ee90a3c0058f83b896f65
LSD_Villax <andresvillabon@gmail.com>
2025-09-27 19:19:06 -0500

    new models

--- soberbio/src/app/app.ts
+++ soberbio/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component} from '@angular/core';
+import { Cliente } from "./model/cliente/cliente";
+import { Comida } from "./model/comida/comida";
+import { Pedido } from "./model/pedido/pedido";
+import { Usuario } from "./model/usuario/usuario";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Cliente, Comida, Pedido, Usuario]
 })
 export class App {
   title = 'mi casa tio';
---
+++ soberbio/src/app/model/comida/comida.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comida',
+  imports: [],
+  templateUrl: './comida.html',
+  styleUrl: './comida.css'
+})
+export class Comida {
+
+}
