f5d42fe0b1995198036c2a6a30159aabdc0a118a
LSD_Villax <andresvillabon@gmail.com>
2025-09-29 19:13:35 -0500

    pagina menu

--- soberbio/src/app/app.routes.ts
+++ soberbio/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './inicio/inicio';
+import { Menu } from './comida/menu/menu';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: Inicio },
+  { path: 'menu', component: Menu },
+  { path: '**', redirectTo: 'inicio' }
+];
